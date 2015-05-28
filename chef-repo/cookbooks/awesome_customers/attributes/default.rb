279331710c86eb533cd843a8ba932f88e1f0acd0
tpetchel <tpetchel@gmail.com>
2015-05-28 14:24:28 -0700

    restore docroot_dir

--- chef-repo/cookbooks/awesome_customers/attributes/default.rb
+++ chef-repo/cookbooks/awesome_customers/attributes/default.rb
@@ -4,6 +4,8 @@ default['awesome_customers']['group'] = 'web_admin'
 default['awesome_customers']['name'] = 'customers'
 default['awesome_customers']['config'] = 'customers.conf'
 
+default['apache']['docroot_dir'] = '/srv/apache/customers'
+
 default['awesome_customers']['passwords']['secret_path'] = '/tmp/encrypted_data_bag_secret'
 
 default['awesome_customers']['database']['dbname'] = 'products'
