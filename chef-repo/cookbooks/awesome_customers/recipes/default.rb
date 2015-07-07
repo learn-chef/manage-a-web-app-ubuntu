b1e68218be65693367bd3650f579c071e56b4c6b
tpetchel <tpetchel@gmail.com>
2015-07-07 16:00:43 -0400

    upgrade firewall cookbook to version 1.5.0
    - the testing tutorial requires a newer version.

--- chef-repo/cookbooks/awesome_customers/recipes/default.rb
+++ chef-repo/cookbooks/awesome_customers/recipes/default.rb
@@ -5,5 +5,6 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 include_recipe 'apt::default'
 include_recipe 'awesome_customers::user'
+include_recipe 'firewall::default'
 include_recipe 'awesome_customers::webserver'
 include_recipe 'awesome_customers::database'
